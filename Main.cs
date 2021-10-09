3452ade31a07efb2aeb0657a195aedf14a5f5313
Miles <smbradtmichael@gmail.com>
2021-10-09 12:07:17 -0700

    initial commit: framework after learning C#

---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Flash2;
+using UnhollowerRuntimeLib;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace DynamicRoll
+{
+    public static class Main
+    {
+        private static SoundController _dynamicRoll;
+        private static CameraController _cameraController;
+
+        public static List<Type> OnModLoad(Dictionary<string, object> settings)
+        {
+            return new List<Type> { typeof(SoundController) };
+        }
+
+        public static void OnModUpdate()
+        {
+            var player = Object.FindObjectOfType<Player>();
+            
+            if (player == null) {
+                Object.Destroy(_dynamicRoll);
+                _dynamicRoll = null;
+                return; 
+            }
+
+            if(_dynamicRoll == null)
+            {
+                _cameraController = player.GetCameraController();
+                _dynamicRoll = new SoundController(_cameraController.gameObject.AddComponent(Il2CppType.Of<SoundController>()).Pointer);
+            }
+        }
+    }
+}
---
+++ SoundController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Flash2;
+using Framework.UI;
+using UnityEngine;
+
+namespace DynamicRoll
+{
+    internal class SoundController : MonoBehaviour
+    {
+        public SoundController(IntPtr value) : base(value) { }
+
+        private Player _player;
+        private CriAtomExPlayer _audio;
+
+        internal void Awake()
+        {
+            _player = FindObjectOfType<Player>();
+
+            // Reads acb file from current directory
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"se_com.acb");
+
+            _audio = new CriAtomExPlayer();
+            
+            CriAtomExAcb acb = CriAtomExAcb.LoadAcbFile(null, path, null);
+            _audio.SetCue(acb, "ballroll");
+            _audio.Start();
+        }
+
+        internal void Update()
+        {
+            var playerSpeed = _player.m_PhysicsBall.m_Velocity.sqrMagnitude * 1000f;
+            var rounded = Math.Round(playerSpeed, 2);
+
+            /*Console.WriteLine(rounded); */
+
+            if (_audio.GetStatus() == CriAtomExPlayer.Status.Playing || _audio.GetStatus() == CriAtomExPlayer.Status.Prep)
+            {
+                return;
+            }
+            else
+            {
+                _audio.SetPitch(playerSpeed);
+                _audio.SetPlaybackRatio(_player.m_PhysicsBall.m_Velocity.sqrMagnitude * 10f);
+                _audio.Update(_audio.Start());
+            }
+        }
+    }
+}
