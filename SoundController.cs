3b3fc4ef21eed80d1bd42e7b0598fdeb04019bf6
Miles <smbradtmichael@gmail.com>
2021-10-09 20:36:27 -0700

    had to redo: ball and spark sounds working in harmony

--- SoundController.cs
+++ SoundController.cs
@@ -12,7 +12,8 @@ namespace DynamicRoll
         public SoundController(IntPtr value) : base(value) { }
 
         private Player _player;
-        private CriAtomExPlayer _audio;
+        private CriAtomExPlayer _ballroll;
+        private CriAtomExPlayer _spark;
 
         internal void Awake()
         {
@@ -21,11 +22,13 @@ namespace DynamicRoll
             // Reads acb file from current directory
             string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"se_com.acb");
 
-            _audio = new CriAtomExPlayer();
+            _ballroll = new CriAtomExPlayer();
+            _spark = new CriAtomExPlayer();
             
             CriAtomExAcb acb = CriAtomExAcb.LoadAcbFile(null, path, null);
-            _audio.SetCue(acb, "ballroll");
-            _audio.Start();
+            _ballroll.SetCue(acb, "ballroll");
+            _spark.SetCue(acb, "1up");
+            
         }
 
         internal void Update()
@@ -33,17 +36,38 @@ namespace DynamicRoll
             var playerSpeed = _player.m_PhysicsBall.m_Velocity.sqrMagnitude * 1000f;
             var rounded = Math.Round(playerSpeed, 2);
 
-            /*Console.WriteLine(rounded); */
+            // My best guess on the formulas they used in the GC games based on audio
+            var pitch = (float)(14.73f * rounded) - 511.08f;
+            var sparkPitch = (float)(14.32f * rounded) - 487.13f;
 
-            if (_audio.GetStatus() == CriAtomExPlayer.Status.Playing || _audio.GetStatus() == CriAtomExPlayer.Status.Prep)
+            /*Console.WriteLine(_player.IsOnGround());*/
+            Console.WriteLine("rolling: " + _ballroll.GetStatus());
+            Console.WriteLine("spark: " + _spark.GetStatus());
+
+            if (_player.IsOnGround() == false || rounded < 4.6f)
+            {
+                _ballroll.Stop();
+                _spark.Stop();
+            }
+
+            
+            if ((_ballroll.GetStatus() == CriAtomExPlayer.Status.Playing || _ballroll.GetStatus() == CriAtomExPlayer.Status.Prep))
             {
                 return;
-            }
-            else
+            } 
+            else if (_player.IsOnGround() && rounded >= 4.6f)
+            { 
+                _ballroll.SetPitch(pitch);
+                _ballroll.Update(_ballroll.Start());
+            } 
+
+            if ((_spark.GetStatus() == CriAtomExPlayer.Status.Playing || _spark.GetStatus() == CriAtomExPlayer.Status.Prep))
             {
-                _audio.SetPitch(playerSpeed);
-                _audio.SetPlaybackRatio(_player.m_PhysicsBall.m_Velocity.sqrMagnitude * 10f);
-                _audio.Update(_audio.Start());
+                return;
+            } else if (_player.IsOnGround() && rounded >= 22.6f)
+            {
+                _spark.SetVolume(0.5f);
+                _spark.Update(_spark.Start()); 
             }
         }
     }
